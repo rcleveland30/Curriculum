7c55d685eaa1df0a69e706660a639a3df4707000
Matthew Day <matt@matthewday.net>
2022-05-30 17:58:57 -0500

    video notes on functions

---
+++ javascript/data/practice1.js
@@ -0,0 +1,41 @@
+// const peopleOnZoom = ['Oscar', 'Whitney', 'Hodan', 'Ron', 'Anna'];
+
+// const myName = "Ron";
+
+// const friendsName = "Sammy"
+
+// const greeting = "Hello, Ron";
+
+// const ezGreeting = "Hello " + myName + " and " + friendsName; //concatination
+
+// const greeting = `Hi, ${myName} and ${friendsName}.`  //string interpolation uses backticks
+
+// console.log(greeting);
+
+// const verb = 'run '
+
+// const noun = 'cat'
+
+// const adj = 'scared'
+
+// const madLib = `I like to ${verb} with my ${noun} when I am ${adj}.`;
+
+// console.log(madLib)
+
+const peopleOnZoom = ['Oscar', 'Whitney', 'Hodan', 'Ron', 'Anna'];
+
+// peopleOnZoom.push ('Bob'); // no "=" for dot push function, adds to end of the array
+
+// peopleOnZoom.pop () // removes item at last index
+
+// peopleOnZoom.length // counts items in array
+
+// console.log(peopleOnZoom.indexOf ('Hodan')) //returns the index of element or negative one if it isn't
+
+const myName = 'Ron Cleveland, II'
+
+const greeting = `My friend\'s\ name is ${myName}`
+
+myName.charAt(6) // identifies the character in that index place
+
+// typeof (data type) = identifies the data type being used
